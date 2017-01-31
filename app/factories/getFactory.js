01efe07dbcf00b5847e06693bdac59025c7dbb15
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-31 09:07:33 -0600

    added comments to getFactory

--- app/factories/getFactory.js
+++ app/factories/getFactory.js
@@ -1,32 +1,41 @@
 app.factory('getFactory', function($http){
     return {
       getService : ()=> {
+        //go get the data
         return $http
           .get("https://data.nashville.gov/resource/8zc7-2afq.json")
           .then((e)=>{
+            //return the data object
             return e.data
           })
+          //take the data object
           .then((e)=>{
+            //create an empty object
             obj = {}
             for(var i = 0; i< e.length; i++) {
+              //for the new object set the type of service as the keys
               let contact_type = e[i].contact_type
-              console.log("e[i].contact_type", e[i].contact_type)
+              //when the service comes up again, add one to the values
               if(obj[contact_type]) {
                 obj[contact_type] += 1;
+                //if the service hasn't been created as an object key yet
+                //create it as an object key and set the initial value to one
               } else {
-                obj[contact_type] = 1
+                obj[contact_type] = 1;
               }
             }
-
+            //return the object of services as the keys with the
+            //count of said services as the value
             return obj
           })
         },
 
     getContacts : ()=> {
         return $http
+        //go get the data
           .get("https://data.nashville.gov/resource/8zc7-2afq.json")
           .then((e)=>{
-
+            //return the data object
             return e.data
           })
         } //end of getContacts()
